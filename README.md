dec76e78f1ef2ef7a1cd6f6182d4244c31f7a8ed
Ryan Rentfro <rrentfro@gmail.com>
2016-07-27 20:43:54 -0700

    Updates the README.md and the bootstrap.sh file with additional cleanup

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ The NET Core 1.0 Boilerplate is a vagrant box that installs Ubuntu 14.04, NET Co
 - Enter the directory
 - In your console type: vagrant up
 - If everything works in the final screen a new NET Core hello world application will be compiled, ran, and output "Hello World!"
-- If you would like to install additional packages you can configure them in vagrant/provision.sh
+- If you would like to install additional packages you can configure them in vagrant/bootstrap.sh
 
 ##Found a Bug or Have a Suggestion?
 Please report your bug or suggestion to the [issues](https://github.com/SourceCode/netcoreboiler/issues) section.
--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -2,25 +2,43 @@
 # Repo: http://github.com/SourceCode/netcoreboiler
 # License: MIT
 
-# This file bootstraps the required repositories
+###################################################################################
+# This file bootstraps the required repositories for NET Core 1.0 on Ubuntu 14.04 #
+###################################################################################
 
+###################################################################################
+# Bootstrap Configurations
+#
+# Note: Mark items as true to install them
+###################################################################################
+couchDB=false
+
+###################################################################################
 # Install essential build tools
+###################################################################################
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install -y build-essential
 
+###################################################################################
 # Allows management of source repositories
+###################################################################################
 sudo apt-get install software-properties-common -y
 
-
+###################################################################################
 # Install unzip and curl
+###################################################################################
 sudo apt-get install -y unzip curl mlocate 
 
+###################################################################################
 # Install Node JS
+###################################################################################
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
+###################################################################################
 # Install NET Core 1.0
+###################################################################################
 sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
 sudo apt-key adv --keyserver apt-mo.trafficmanager.net --recv-keys 417A0893
 sudo apt-get update
@@ -33,22 +51,61 @@ if [ ! -d "hwapp" ]; then
 	dotnet new
 	dotnet restore
 	dotnet run
+else
+	echo 'It looks like you have already provisioned this box'
+	echo 'If your hwapp dotnet app still exists we will test it now'
+	dotnet restore
+	dotnet run
 fi
 
-# Optional Installs
-# You can uncomment blocks to install the daemon, service, or software as needed
+###################################################################################
+# Optional Package Installation Scripts
+#
+# Note: Packages are configured in the configuration area above
+###################################################################################
 
-# Apache CouchDB
-# sudo add-apt-repository ppa:couchdb/stable -y
-# sudo apt-get update
-# sudo apt-get remove couchdb couchdb-bin couchdb-common -yf
-# sudo apt-get install couchdb -y
-# curl localhost:5984
+#+++++++++++++++++++++++++++++
+# CouchDB
+#+++++++++++++++++++++++++++++
+if [ "$couchDB" = true ] ; then
+    sudo add-apt-repository ppa:couchdb/stable -y
+	sudo apt-get update
+	sudo apt-get remove couchdb couchdb-bin couchdb-common -yf
+	sudo apt-get install couchdb -y
+	curl localhost:5984
+fi
 
+#+++++++++++++++++++++++++++++
+# Nginx
+#+++++++++++++++++++++++++++++
+
+#+++++++++++++++++++++++++++++
+# Apache2
+#+++++++++++++++++++++++++++++
+
+#+++++++++++++++++++++++++++++
+# MongoDB
+#+++++++++++++++++++++++++++++
+
+#+++++++++++++++++++++++++++++
+# Redis
+#+++++++++++++++++++++++++++++
+
+#+++++++++++++++++++++++++++++
+# MySQL
+#+++++++++++++++++++++++++++++
+
+#+++++++++++++++++++++++++++++
+# Cassandra
+#+++++++++++++++++++++++++++++
+
+###################################################################################
 # Update Locate Command
-echo ' '
+###################################################################################
 echo 'Updating locate command cache'
 sudo updatedb
 
+###################################################################################
 # Everything is finished
+###################################################################################
 echo "Everything is built and running. Where would you like to go today?"
